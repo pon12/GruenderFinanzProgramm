a690d03ecdafb0b643e170a3fa7dec619d6acac8
Alexander Ratzka <83948559+ubshf@users.noreply.github.com>
2026-04-10 15:01:05 +0200

    Kostenrechnung

--- GruenderFinanzProgramm/Assets/Backend/Formeln/Kostenrechnung.cs
+++ GruenderFinanzProgramm/Assets/Backend/Formeln/Kostenrechnung.cs
@@ -2,5 +2,52 @@ using UnityEngine;
 
 public static class Kostenrechnung
 {
-    
+    public static float gesamtKosten(float fixKosten, float varKosten)
+    {
+        return fixKosten + varKosten;
+    }
+
+    public static float varKosten(float varKostenProEinheit, int menge)
+    {
+        if (menge < 0)
+        {
+            Debug.LogError("Die Menge darf nicht negativ sein.");
+            return 0f;
+        }
+
+        return varKostenProEinheit * menge;
+    }
+
+    public static float durchschnittlicheFixKosten(float fixKosten, int menge)
+    {
+        if (menge <= 0)
+        {
+            Debug.LogError("Die Menge muss größer als 0 sein.");
+            return 0f;
+        }
+
+        return fixKosten / menge;
+    }
+
+    public static float durchschnittlicheVarKosten(float varKosten, int menge)
+    {
+        if (menge <= 0)
+        {
+            Debug.LogError("Die Menge muss größer als 0 sein.");
+            return 0f;
+        }
+
+        return varKosten / menge;
+    }
+
+    public static float stueckKosten(float gesamtKosten, int menge)
+    {
+        if (menge <= 0)
+        {
+            Debug.LogError("Die Menge muss größer als 0 sein.");
+            return 0f;
+        }
+
+        return gesamtKosten / menge;
+    }
 }
