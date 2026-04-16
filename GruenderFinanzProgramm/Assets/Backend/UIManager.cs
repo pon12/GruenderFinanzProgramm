aea00344839e7ff9e91cb8e0228395789dfe0c39
Alexander Ratzka <83948559+ubshf@users.noreply.github.com>
2026-04-16 18:00:33 +0200

    UIManager anlegen

--- GruenderFinanzProgramm/Assets/Backend/UIManager.cs
+++ GruenderFinanzProgramm/Assets/Backend/UIManager.cs
@@ -3,12 +3,45 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-    void Start()
+
+    private GameObject mainMenuPanel;
+    private GameObject[] userMenuPanels;
+    private GameObject zukünftigeErweiterungenPanel;
+
+    private void Start()
     {
-        
+        showMainMenu();
     }
-    
 
+    public void hideAllMenus()
+    {
+        mainMenuPanel.SetActive(false);
 
+        // userMenuPanel.setActive(false);
+        foreach (GameObject panel in userMenuPanels)
+        {
+            panel.SetActive(false);
+        }
+
+        zukünftigeErweiterungenPanel.SetActive(false);
+
+    }
+
+    public void showMainMenu()
+    {
+        hideAllMenus();
+        mainMenuPanel.SetActive(true);
+    }
+
+    public void showUserMenu()
+    {
+        hideAllMenus();
+
+    }
+
+    public void ShowZukünftigeErweiterungen()
+    {
+
+    }
 
 }
