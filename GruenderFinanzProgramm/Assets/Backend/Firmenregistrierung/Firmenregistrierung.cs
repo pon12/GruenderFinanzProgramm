e96fb8e00442c0e283455edad32fafcd8607a29d
Rattenvieh <schubartnoah@gmail.com>
2026-05-01 18:55:46 +0200

    Datei nochmal gelöscht und neu erstellt
    Visual Studio wurde nicht als Code Editor erkannt aber jetzt gehts

--- GruenderFinanzProgramm/Assets/Backend/Firmenregistrierung/Firmenregistrierung.cs
+++ GruenderFinanzProgramm/Assets/Backend/Firmenregistrierung/Firmenregistrierung.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class sdlfhjaskjhaf : MonoBehaviour
+public class Firmenregistrierung : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
