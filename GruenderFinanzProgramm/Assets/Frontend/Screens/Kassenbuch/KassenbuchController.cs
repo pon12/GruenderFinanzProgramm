a4b79ba07b381b8c8a81c88484530bcd06b3d5d1
Alexander Ratzka <83948559+ubshf@users.noreply.github.com>
2026-06-17 09:54:03 +0200

    Alte Testdaten-Erstellung und PDF-Pfad bereinigen

--- GruenderFinanzProgramm/Assets/Frontend/Screens/Kassenbuch/KassenbuchController.cs
+++ GruenderFinanzProgramm/Assets/Frontend/Screens/Kassenbuch/KassenbuchController.cs
@@ -3,9 +3,6 @@ using UnityEngine.UIElements;
 using System.Collections.Generic;
 using System.Linq;
 using System.Globalization;
-using System.Collections.Generic;
-using System.Linq;
-using System.Globalization;
 
 public class KassenbuchController : MonoBehaviour
 {
