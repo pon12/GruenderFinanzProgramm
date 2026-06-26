a0fcc85756db5b323679fee5cae1c4e700f2b664
0Mars0 <marcelglasser@gmail.com>
2026-06-26 13:06:38 +0200

    Tutorial Script

---
+++ GruenderFinanzProgramm/Assets/Frontend/Skripts/Tutorial.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+using UnityEngine.UI;
+public class TutorialManager : MonoBehaviour
+{
+    [Header("Tutorial-Bilder")]
+    [SerializeField] private Sprite[] tutorialBilder;
+    [Header("UI-Referenzen")]
+    [SerializeField] private GameObject startDialog;
+    [SerializeField] private GameObject tutorialPanel;
+    [SerializeField] private Image tutorialBildAnzeige;
+    [SerializeField] private Button jaButton;
+    [SerializeField] private Button neinButton;
+    [SerializeField] private Button weiterButton;
+    [SerializeField] private Button zurueckButton;
+    private int aktuellerIndex = 0;
+    void Start()
+    {
+        // Start-Dialog anzeigen, Tutorial verstecken
+        startDialog.SetActive(true);
+        tutorialPanel.SetActive(false);
+        // Button-Events registrieren
+        jaButton.onClick.AddListener(TutorialStarten);
+        neinButton.onClick.AddListener(TutorialSchliessen);
+        weiterButton.onClick.AddListener(NaechstesBild);
+        zurueckButton.onClick.AddListener(VorherigesBild);
+    }
+    private void TutorialStarten()
+    {
+        startDialog.SetActive(false);
+        tutorialPanel.SetActive(true);
+        aktuellerIndex = 0;
+        BildAktualisieren();
+    }
+    private void TutorialSchliessen()
+    {
+        startDialog.SetActive(false);
+        tutorialPanel.SetActive(false);
+    }
+    private void NaechstesBild()
+    {
+        if (aktuellerIndex < tutorialBilder.Length - 1)
+        {
+            aktuellerIndex++;
+            BildAktualisieren();
+        }
+        else
+        {
+            // Letztes Bild erreicht – Tutorial beenden
+            TutorialSchliessen();
+        }
+    }
+    private void VorherigesBild()
+    {
+        if (aktuellerIndex > 0)
+        {
+            aktuellerIndex--;
+            BildAktualisieren();
+        }
+    }
+    private void BildAktualisieren()
+    {
+        tutorialBildAnzeige.sprite = tutorialBilder[aktuellerIndex];
+        // Zurück-Button nur anzeigen, wenn nicht beim ersten Bild
+        zurueckButton.interactable = aktuellerIndex > 0;
+        // Optional: Weiter-Button-Text ändern beim letzten Bild
+        var weiterText = weiterButton.GetComponentInChildren<Text>();
+        if (weiterText != null)
+        {
+            weiterText.text = aktuellerIndex >= tutorialBilder.Length - 1 ? "Fertig" : "Weiter";
+        }
+    }
+}
