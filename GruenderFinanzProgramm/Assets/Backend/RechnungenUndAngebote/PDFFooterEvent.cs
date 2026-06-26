4f5ef764344a212770243ac1e9b218083e7469ce
Josef Peinelt <josef08peinelt@gmail.com>
2026-06-26 05:31:52 +0200

    Fix angebot und Rechnung
    Das meiste gefixed, musste alles nochmal wiederherstellen

--- GruenderFinanzProgramm/Assets/Backend/RechnungenUndAngebote/PDFFooterEvent.cs
+++ GruenderFinanzProgramm/Assets/Backend/RechnungenUndAngebote/PDFFooterEvent.cs
@@ -7,58 +7,103 @@ public class PdfFooterEvent : PdfPageEventHelper
 {
     private readonly string firma;
     private readonly string adresse;
+    private readonly bool   showBankData;
+
     private readonly iTextSharp.text.Font footerFont;
 
-    private readonly bool showBankData;
+    // Bankdaten aus PlayerPrefs
+    private readonly string kreditinstitut;
+    private readonly string iban;
+    private readonly string bic;
+    private readonly string kontoinhaber;
 
-public PdfFooterEvent(string firma, string adresse, bool showBankData)
-{
-    this.firma = firma;
-    this.adresse = adresse;
-    this.showBankData = showBankData;
+    // Steuerdaten aus DB (Company)
+    private readonly string steuerNr;
+    private readonly string ustIdNr;
 
-    footerFont = FontFactory.GetFont(
-        FontFactory.HELVETICA,
-        8,
-        iTextSharp.text.Color.BLACK
-    );
-}
+    public PdfFooterEvent(string firma, string adresse, bool showBankData)
+    {
+        this.firma       = firma;
+        this.adresse     = adresse;
+        this.showBankData = showBankData;
+
+        footerFont = FontFactory.GetFont(
+            FontFactory.HELVETICA,
+            8,
+            iTextSharp.text.Color.BLACK
+        );
+
+        // Bankdaten aus PlayerPrefs lesen
+        kreditinstitut = PlayerPrefs.GetString("settings_kreditinstitut", "");
+        iban           = PlayerPrefs.GetString("settings_iban",           "");
+        bic            = PlayerPrefs.GetString("settings_bic",            "");
+        kontoinhaber   = PlayerPrefs.GetString("settings_kontoinhaber",   "");
+
+        // Steuerdaten aus DB lesen (werden nicht in PlayerPrefs gespeichert)
+        steuerNr = "";
+        ustIdNr  = "";
+        try
+        {
+            var db = UserDatabaseAccess.getCurrentUserDatabase();
+            if (db != null)
+            {
+                var companies = db.getAllCompanies();
+                if (companies != null && companies.Count > 0)
+                {
+                    steuerNr = companies[0].steuerNr   ?? "";
+                    ustIdNr  = companies[0].ustIdNr    ?? "";
+
+                    // Firmenname und Adresse aus DB als Fallback wenn nicht übergeben
+                    if (string.IsNullOrEmpty(this.firma))
+                        this.firma = companies[0].name ?? "";
+                    if (string.IsNullOrEmpty(this.adresse))
+                    {
+                        string ort = companies[0].location         ?? "";
+                        string str = companies[0].strasseuHausNr   ?? "";
+                        string plz = companies[0].plz > 0
+                            ? companies[0].plz.ToString()
+                            : "";
+                        this.adresse = str + "\n" + plz + " " + ort;
+                    }
+                }
+            }
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogWarning("[PdfFooterEvent] DB-Zugriff fehlgeschlagen: " + e.Message);
+        }
+    }
 
     public override void OnEndPage(PdfWriter writer, Document document)
     {
         PdfPTable footerTable = new PdfPTable(3);
-        footerTable.TotalWidth = document.PageSize.Width - document.LeftMargin - document.RightMargin;
+        footerTable.TotalWidth =
+            document.PageSize.Width - document.LeftMargin - document.RightMargin;
         footerTable.SetWidths(new float[] { 1.4f, 1.4f, 1.2f });
 
-        AddFooterCell(
-            footerTable,
-            firma + "\n" + adresse,
-            footerFont
-        );
+        // Spalte 1: Firmenname + Adresse
+        AddFooterCell(footerTable, firma + "\n" + adresse, footerFont);
 
+        // Spalte 2: Bankverbindung
         string bankText =
             "Bankverbindung\n" +
-            PlayerPrefs.GetString("settings_kreditinstitut", "") + "\n" +
-            "IBAN: " + PlayerPrefs.GetString("settings_iban", "") + "\n" +
-            "BIC: " + PlayerPrefs.GetString("settings_bic", "") + "\n" +
-            "Konto-Inh.: " + PlayerPrefs.GetString("settings_kontoinhaber", "");
+            kreditinstitut + "\n" +
+            "IBAN: " + iban + "\n" +
+            "BIC: "  + bic  + "\n" +
+            "Konto-Inh.: " + kontoinhaber;
 
-        AddFooterCell(
-            footerTable,
-            bankText,
-            footerFont
-        );
+        AddFooterCell(footerTable, bankText, footerFont);
 
-        AddFooterCell(
-            footerTable,
+        // Spalte 3: Steuerdaten
+        string steuerText =
             "Steuerdaten\n" +
-            "Steuer-Nr.: " + PlayerPrefs.GetString("settings_steuernummer", "") + "\n" +
-            "USt-IdNr.: " + PlayerPrefs.GetString("settings_ustidnr", ""),
-            footerFont
-        );
+            "Steuer-Nr.: " + steuerNr + "\n" +
+            "USt-IdNr.: "  + ustIdNr;
 
+        AddFooterCell(footerTable, steuerText, footerFont);
+
+        // Trennlinie
         PdfContentByte canvas = writer.DirectContent;
-
         float lineY = document.BottomMargin - 5;
         canvas.SetLineWidth(0.8f);
         canvas.MoveTo(document.LeftMargin, lineY);
@@ -66,8 +111,7 @@ public PdfFooterEvent(string firma, string adresse, bool showBankData)
         canvas.Stroke();
 
         footerTable.WriteSelectedRows(
-            0,
-            -1,
+            0, -1,
             document.LeftMargin,
             document.BottomMargin - 12,
             canvas
@@ -81,10 +125,10 @@ public PdfFooterEvent(string firma, string adresse, bool showBankData)
     )
     {
         PdfPCell cell = new PdfPCell(new Phrase(text, font));
-        cell.Border = Rectangle.NO_BORDER;
-        cell.PaddingTop = 4;
-        cell.PaddingRight = 10;
-        cell.HorizontalAlignment = Element.ALIGN_LEFT;
+        cell.Border               = Rectangle.NO_BORDER;
+        cell.PaddingTop           = 4;
+        cell.PaddingRight         = 10;
+        cell.HorizontalAlignment  = Element.ALIGN_LEFT;
         table.AddCell(cell);
     }
 }
