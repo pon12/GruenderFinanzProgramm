a753ee3f6a6395ab5e86e6ef5d49e591fca1c7ad
Noah <nschubar@hs-mittweida.de>
2026-08-10 19:19:57 +0200

    Präfix Fix

--- GruenderFinanzProgramm/Assets/Frontend/Skripts/RechnungController.cs
+++ GruenderFinanzProgramm/Assets/Frontend/Skripts/RechnungController.cs
@@ -5,7 +5,8 @@ using UnityEngine.UIElements;
 public class RechnungController : BelegScreenController
 {
     protected override string BelegTyp      => "Rechnung";
-    protected override string NummernPrefix => "RG-NR";
+    protected override string NummernPrefix =>
+        PlayerPrefs.GetString("settings_rechnr_praefix", "RG-NR");
 
     protected override List<string> StatusOptionen => new List<string>
     {
